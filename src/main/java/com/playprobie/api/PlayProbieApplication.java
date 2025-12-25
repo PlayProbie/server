53920104beb5c67b2f8be7513d5e27a805a09026
Hyun <itsmine096@gmail.com>
2025-12-26 01:45:26 +0900

    feat: 프로젝트 생성 및 환경 분리 (#2)
    * feat: Spring Boot 3.5.9 프로젝트 기본 구조 생성
    
    * feat: 환경별 프로필 분리 및 Actuator(Health Check) 의존성 추가

---
+++ src/main/java/com/playprobie/api/PlayProbieApplication.java
@@ -0,0 +1,13 @@
+package com.playprobie.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayProbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayProbieApplication.class, args);
+	}
+
+}
