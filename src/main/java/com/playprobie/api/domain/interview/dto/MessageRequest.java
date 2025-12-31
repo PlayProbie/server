7479136c0d3890176a310b7c941ad83f11d0f1fa
YongHwan <clazziquai01@gmail.com>
2026-01-01 00:44:17 +0900

    chore: DTO 및 사용하지 않는 클래스 제거
    - Rename 및 사용하지 않는 클래스 제거

--- src/main/java/com/playprobie/api/domain/interview/dto/MessageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.playprobie.api.domain.interview.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record MessageRequest(
-        @JsonProperty("turn_num") Integer turnNum,
-
-        @JsonProperty("answer_text") String answerText) {
-}
