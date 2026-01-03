a53d4f3bedfe80bc24098da76815538d4dda2edb
Hyun <itsmine096@gmail.com>
2026-01-04 01:34:43 +0900

    build: AWS 설정 및 의존성 추가
    - AwsConfig, AwsProperties 추가
    - build.gradle에 AWS SDK 의존성 추가
    - application-dev.yml에 AWS 환경변수 설정 추가

---
+++ src/main/java/com/playprobie/api/infra/config/AwsConfig.java
@@ -0,0 +1,52 @@
+package com.playprobie.api.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.gamelift.GameLiftClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class AwsConfig {
+
+    private final AwsProperties awsProperties;
+
+    @Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                        awsProperties.getAccessKey(),
+                        awsProperties.getSecretKey()));
+    }
+
+    @Bean
+    public S3Client s3Client(AwsCredentialsProvider credentialsProvider) {
+        return S3Client.builder()
+                .region(Region.of(awsProperties.getRegion()))
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    @Bean(destroyMethod = "close")
+    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider) {
+        return S3Presigner.builder()
+                .region(Region.of(awsProperties.getRegion()))
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    @Bean
+    public GameLiftClient gameLiftClient(AwsCredentialsProvider credentialsProvider) {
+        return GameLiftClient.builder()
+                .region(Region.of(awsProperties.getRegion()))
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+}
